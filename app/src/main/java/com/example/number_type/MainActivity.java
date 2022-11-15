33b12d6d695a399b6f20015377766e39769b8a43
abdulrab chatha <abdulrabchatha@gmail.com>
2022-11-15 09:00:05 +0500

    final color grading

--- app/src/main/java/com/example/number_type/MainActivity.java
+++ app/src/main/java/com/example/number_type/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.number_type;
 
+import static com.example.number_type.R.id.Result_View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -14,9 +16,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     int rand_num ;
-    TextView rand_txt, Result_view, right_view, wrong_view;
+    TextView rand_txt, Result_view, right_view, wrong_view,result, wrong, right;
     int random, num;
     Button odd,even,prime;
+
 
 
 
@@ -25,55 +28,73 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rand_txt = findViewById(R.id.Random_num);
-        Result_view = findViewById(R.id.Result_View);
+        Result_view = findViewById(Result_View);
         right_view = findViewById(R.id.Right_view);
         wrong_view = findViewById(R.id.Wrong_view);
+        result = findViewById(Result_View);
+        wrong = findViewById(R.id.EVEN_Score2);
+        right = findViewById(R.id.EVEN_Score);
         odd = findViewById(R.id.button);
+        odd.setBackgroundColor(Color.RED);
+        odd.setTextColor(Color.BLACK);
+
+        right.setTextColor(Color.GREEN);
+        wrong.setTextColor(Color.RED);
         odd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (random % 2 != 0)
                 {
                     Result_view.setText("SUCCESS");
+                    Result_view.setTextColor(Color.GREEN);
                     Success_Increment();
                 }
                 else
                 {
                     Result_view.setText("FAILURE");
+                    Result_view.setTextColor(Color.RED);
                     Failure_Increment();
                 }
                 generate_rand_no();
             }
         });
         even = findViewById(R.id.button2);
+        even.setBackgroundColor(Color.YELLOW);
+        even.setTextColor(Color.BLACK);
         even.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (random % 2 == 0)
                 {
                     Result_view.setText("SUCCESS");
+                    Result_view.setTextColor(Color.GREEN);
                     Success_Increment();
                 }
                 else
                 {
                     Result_view.setText("FAILURE");
+                    Result_view.setTextColor(Color.RED);
                     Failure_Increment();
                 }
                 generate_rand_no();
             }
         });
         prime = findViewById(R.id.button3);
+        prime.setBackgroundColor(Color.GREEN);
+        prime.setTextColor(Color.BLACK);
         prime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isPrime(random))
                 {
                     Result_view.setText("SUCCESS");
+                    Result_view.setTextColor(Color.GREEN);
                     Success_Increment();
                 }
                 else
                 {
                     Result_view.setText("FAILURE");
+                    Result_view.setTextColor(Color.RED);
                     Failure_Increment();
                 }
                 generate_rand_no();
